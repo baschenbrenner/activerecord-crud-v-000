e009aa132fe44a5a3782417e6d3bf93577a36f4f
Benjamin Aschenbrenner <baschenbrenner@hotmail.com>
2017-11-01 01:41:30 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -84,7 +84,7 @@ def can_update_multiple_items_at_once
     Movie.create(title: "Movie_#{i}", release_date: 2000+i)
   end
   movies = {0 => {:title=>"A Movie"}, 1 => {:title=>"A Movie"}, 2 => {:title=>"A Movie"}, 3 => {:title=>"A Movie"}, 4 => {:title=>"A Movie"}}
-  Movie.update()
+  Movie.update(movies.keys, movies.values)
 end
 
 def can_destroy_a_single_item
