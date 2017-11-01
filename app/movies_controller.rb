d2bfe4b8dadbab1bcb61700dfbabbc27b8601cd4
Benjamin Aschenbrenner <baschenbrenner@hotmail.com>
2017-11-01 00:56:14 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -57,8 +57,8 @@ end
 def can_find_using_where_clause_and_be_sorted
   new_array=Movie.all.select {|movie| movie.release_date > 2002}
   new_array.sort {|a.release_date,b.release_date| b<=>a}
-  ActiveRecord::Base.connection.execute("SELECT * FROM movies WHERE release_date > 2002 ORDER BY release_date DESC;")
-  binding.pry
+  #ActiveRecord::Base.connection.execute("SELECT * FROM movies WHERE release_date > 2002 ORDER BY release_date DESC;")
+  #binding.pry
 end
 
 def can_be_found_updated_and_saved
